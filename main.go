de47fb4076c11084ea52c9a49d64f40138e50410
Eric Lim <cowboy-bebug@users.noreply.github.com>
2025-03-01 22:43:25 +1300

    feat(cmd): update versioning information in main.go
    - Change import path from 'kommit' to 'kommitment'
    - Add version, commit, and date variables to main function
    - Set cmd.Version, cmd.Commit, and cmd.Date with respective values
    
    [Therapy notes by Kommitment - github.com/cowboy-bebug/kommitment]

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"github.com/cowboy-bebug/kommit/cmd"
+	"github.com/cowboy-bebug/kommitment/cmd"
+)
+
+var (
+	version = "unknown"
+	commit  = "unknown"
+	date    = "unknown"
 )
 
 func main() {
+	cmd.Version = version
+	cmd.Commit = commit
+	cmd.Date = date
 	cmd.Execute()
 }
